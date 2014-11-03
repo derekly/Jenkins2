3ae95d54a96f2b891aa6d99a59fd493ae6eacbda
Derek Ly <Athena@wireless-10-147-147-147.public.utexas.edu>
2014-11-03 15:17:57 -0600

    creating failing test

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -32,7 +32,7 @@ class MyTestCase(unittest.TestCase):
 
   def test_t8(self):
     r1 = Experiment.largest([1, 2, 3, 4, 5]) 
-    self.assertEqual(r1, 5)
+    self.assertEqual(r1, 4)
 
   def test_t9(self):
     r1 = Experiment.largest([1, 0, -1, -2]) 
